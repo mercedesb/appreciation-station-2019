f6fbf5988a9d5402c936a9783ad837b3935ab11f
Mercedes Bernard <mercedesrbernard@gmail.com>
2019-05-26 11:02:53 -0500

    Add intial first year statistics
    Add ByTheNumbers component to display some stats from our first year.
    Created a function to render each stat from an array.
    Updated styles to make stats overlap the cover and still look correct on
    tablet and mobile.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ThankYouCover from "./components/ThankYouCover";
+import ByTheNumbers from "./components/ByTheNumbers";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <React.Fragment>
       <ThankYouCover />
+      <div className="Section">
+        <ByTheNumbers />
+      </div>
+
       <Footer />
     </React.Fragment>
   );
---
+++ src/components/ByTheNumbers.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+export default function ByTheNumbers() {
+  const YELLOW = "#FFF7E4";
+  const BLUE = "#EBF3FB";
+  const RED = "#F7EAEA";
+  const ORANGE = "#EBF3FB";
+  const GREEN = "#a9dfce";
+
+  const statistics = [
+    {
+      number: 19,
+      text: "Events held across our Chicago and Madison chapters.",
+      color: YELLOW
+    },
+    {
+      number: 218,
+      text: "Different people attended our Chicago events.",
+      color: BLUE
+    },
+    {
+      number: 74,
+      text:
+        "Projects reviewed and pair programmed on by our mentors and mentees.",
+      color: RED
+    },
+    {
+      number: 42,
+      text: "Mentors attended our Chicago Code Review & Pairing events.",
+      color: ORANGE
+    },
+    {
+      number: 66,
+      text: "Mentees attended our Chicago Code Review & Pairing events.",
+      color: GREEN
+    }
+  ];
+
+  const renderStatistic = (number, text, color) => {
+    return (
+      <div class="GridItem GridItem--third Statistic">
+        <div class="Statistic-background">
+          <svg x="0px" y="0px" viewBox="0 0 54 54">
+            <circle
+              fill={color}
+              stroke={color}
+              stroke-width="1"
+              cx="27"
+              cy="27"
+              r="25"
+              stroke-dasharray="157 157"
+              stroke-dashoffset="157"
+            />
+          </svg>
+        </div>
+        <div class="Statistic-number">{number}</div>
+        <p class="Statistic-text">{text}</p>
+      </div>
+    );
+  };
+
+  return (
+    <div className="Grid StatisticsContainer">
+      {statistics.map(stat =>
+        renderStatistic(stat.number, stat.text, stat.color)
+      )}
+    </div>
+  );
+}
