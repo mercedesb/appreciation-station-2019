5f507750f3497783b33059a367dfb6659d251694
Mercedes Bernard <mercedesrbernard@gmail.com>
2019-05-26 09:56:19 -0500

    Add initial navigation
    Create Navigation component to hold any navigation needed for this app.
    Currently, just show the Dev Together logo.

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Dev_TogetherLogoWhite from "../assets/images/Dev_TogetherLogoWhite.svg";
+
+export default function Navigation() {
+  return (
+    <nav>
+      <span class="Logo">
+        <img src={Dev_TogetherLogoWhite} alt="Dev Together logo" />
+      </span>
+    </nav>
+  );
+}
