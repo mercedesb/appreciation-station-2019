ff9ce703973f279656bc620154bf84ed03a97e32
Mercedes Bernard <mercedesrbernard@gmail.com>
2019-06-09 08:40:01 -0500

    Add boilerplate for first render test of functional component
    Add test file, imports, and initial mock data for our first workshop
    activity.

---
+++ src/components/__tests__/AppreciationStation.test.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+
+import AppreciationStation from "../AppreciationStation";
+
+const mockThankYous = [
+  {
+    member: {
+      name: "Test member 1",
+      isMentor: true
+    },
+    message: {
+      text: "Thank you {NAME} for being a great mentor!"
+    },
+    backgroundImage: {
+      file: {
+        url: "http://testurl1.com"
+      }
+    }
+  },
+  {
+    member: {
+      name: "Test member 2",
+      isMentee: true
+    },
+    message: {
+      text: "Thank you {NAME} for being a great mentee!"
+    },
+    backgroundImage: {
+      file: {
+        url: "http://testurl2.com"
+      }
+    }
+  },
+  {
+    member: {
+      name: "Test member 3",
+      isSpeaker: true
+    },
+    message: {
+      text: "Thank you {NAME} for being a great speaker!"
+    },
+    backgroundImage: {
+      file: {
+        url: "http://testurl3.com"
+      }
+    }
+  },
+  {
+    member: {
+      name: "Test member 4",
+      isSponsor: true
+    },
+    message: {
+      text: "Thank you {NAME} for being a great sponsor!"
+    },
+    backgroundImage: {
+      file: {
+        url: "http://testurl4.com"
+      }
+    }
+  }
+];
+
+// automatically unmount and cleanup DOM after the test is finished.
+afterEach(cleanup);
