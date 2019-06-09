4ed01d5d307159d94a0e1c0fa66bbd78e11c507a
Mercedes Bernard <mercedesrbernard@gmail.com>
2019-06-09 08:03:05 -0500

    Add our first unit test for `sum(number, number)`
    Added a sum function in `App.test.js`
    
    Tested various number inputs as well as error handling.

--- src/App.test.js
+++ src/App.test.js
@@ -14,3 +14,33 @@ https://github.com/testing-library/react-testing-library/issues/281
 //   ReactDOM.render(<App />, div);
 //   ReactDOM.unmountComponentAtNode(div);
 // });
+
+function sum(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Both arguments must be numbers");
+  }
+
+  return a + b;
+}
+
+describe("sum", () => {
+  it("should add 2 positive numbers", () => {
+    expect(sum(2, 3)).toEqual(5);
+  });
+
+  it("should add 2 negative numbers", () => {
+    expect(sum(-2, -3)).toEqual(-5);
+  });
+
+  it("should add 1 positive and 1 negative number", () => {
+    expect(sum(-2, 3)).toEqual(1);
+  });
+
+  it("should add a number and 0", () => {
+    expect(sum(2, 0)).toEqual(2);
+  });
+
+  it("should raise an error when trying to add something that's not a number", () => {
+    expect(() => sum("0", 3)).toThrowError("Both arguments must be numbers");
+  });
+});
