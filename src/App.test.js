9a5f4a9b37f0236efa1f4526cd3ed5b2376deace
Mercedes Bernard <mercedesrbernard@gmail.com>
2019-06-09 13:16:31 -0500

    Comment out default App test
    Commented out default test due to but in React <16.9. The big
    red warning is very distracting for the workshop.
    Bug documented here: https://github.com/testing-library/react-testing-library/issues/281

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+/* 
+Commented out default test due to but in React <16.9. The big 
+red warning is very distracting for the workshop.
+Bug documented here:
+https://github.com/testing-library/react-testing-library/issues/281
+*/
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
